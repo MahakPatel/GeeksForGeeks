23937a750b4f69ad85d3a4a6f02bfec1b2a85612
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-06-27 03:22:31 -0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Delete all occurrences of a given key in a doubly linked list/delete-all-occurrences-of-a-given-key-in-a-doubly-linked-list.cpp
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+
+    new_node->data = new_data;
+
+    new_node->prev = NULL;
+
+    new_node->next = (*head_ref);
+
+    if ((*head_ref) != NULL) (*head_ref)->prev = new_node;
+
+    (*head_ref) = new_node;
+}
+
+void printList(struct Node* head) {
+    if (head == NULL) cout << -1;
+
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+
+/* a Node of the doubly linked list 
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
+class Solution {
+  public:
+    void deleteAllOccurOfX(struct Node** head_ref, int x) {
+        Node *temp = *head_ref;
+        
+        while(temp != NULL){ // check untill last node
+            if(temp -> data == x){ // if LL data is same then the given key 
+                if(temp == *head_ref){ // if head value is same as given key
+                     *head_ref = temp -> next; // second node become new head
+                }
+                Node *nextNode = temp-> next; // nextnode will be the temo of next
+                Node *prevNode = temp-> prev; // previous node is aleays temp-> prev
+                 
+                if(nextNode) nextNode-> prev = prevNode; // if it have next node (is condition is not for lst node)
+                if(prevNode) prevNode-> next = nextNode; // if it have previous node (is condition is not for head node)
+                
+                free(temp);
+                temp = nextNode; // move temp to nextNode
+            }else{
+                temp = temp -> next; // if value is not same then move temp to next noide
+            }
+            
+        }
+        
+        return;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        Node* head = new Node();
+        head = NULL;
+
+        int n;
+        cin >> n;
+
+        vector<int> inp(n);
+
+        for (int i = 0; i < n; i++) {
+            int val;
+            cin >> val;
+            // push(&head,val);
+            inp[i] = val;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            push(&head, inp[i]);
+        }
+        int key;
+        cin >> key;
+        // printList(head);
+        // cout << endl;
+
+        Solution obj;
+
+        obj.deleteAllOccurOfX(&head, key);
+        printList(head);
+        cout << endl;
+
+        // cout << "\n~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
