aaa413387e68e5bcba3abd4464f297daf889c3cb
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-11-09 20:20:46 -0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+  // +++++++++++++ BFS +++++++++++ TC O(N * 100000).    SC O(100000)
+    int minimumMultiplications(vector<int>& arr, int start, int end) {
+        // Create a queue for storing the numbers as a result of multiplication
+        // of the numbers in the array and the start number.
+        queue<pair<int, int>> q;
+        q.push({start, 0});
+        
+        if (start == end) return 0;
+
+        // Create a dist array to store the no. of multiplications to reach
+        // a particular number from the start number.
+        vector<int> dist(100000, 1e9);
+        dist[start] = 0;
+        int mod = 100000;
+
+        // Multiply the start no. with each of numbers in the arr
+        // until we get the end no.
+        while (!q.empty())
+        {
+            int node = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+
+            for (auto it : arr)
+            {
+                int num = (it * node) % mod;
+
+                // If the no. of multiplications are less than before
+                // in order to reach a number, we update the dist array.
+                if (steps + 1 < dist[num])
+                {
+                    dist[num] = steps + 1;
+
+                    // Whenever we reach the end number
+                    // return the calculated steps
+                    if (num == end)
+                        return steps + 1;
+                    q.push({num, steps + 1});
+                }
+            }
+        }
+        // If the end no. is unattainable.
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        int start, end;
+        cin >> start >> end;
+        Solution obj;
+        cout << obj.minimumMultiplications(arr, start, end) << endl;
+    
+cout << "~" << "\n";
+}
+}
+
+// } Driver Code Ends
