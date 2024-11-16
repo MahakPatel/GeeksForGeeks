3bc5e99774da656a84393fffcee76dfcaa20e42d
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-11-16 17:54:08 -0600

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
@@ -6,39 +6,17 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  // +++++++++++++++++++ Brute force  ++++++++++++++++
-  // TC O(N).   SC O(N)
- 
-//Function to reverse an array
-    vector<int> reverseArray(vector<int> &arr) {
-        int n = arr.size();        // Get the size of the input array
-        vector<int> ans;           // Create a new vector to hold the reversed array
+    void reverseArray(vector<int> &arr) {
+        int n = arr.size();
+        int i=0;
+        int j= n-1;
         
-        // Iterate from the last index to the first index
-        for (int i = n - 1; i >= 0; i--) {
-            ans.push_back(arr[i]); // Add the elements in reverse order
+        while(i< j){
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
         }
-        
-        return ans;                // Return the reversed array
     }
-    
-    
-    // +++++++++++ Optimal Solution TC O(nlogn).  SC O(1) +++++++++++++++
-    
-    //  // Function to reverse an array in-place
-    // void reverseArray(vector<int>& arr) {
-    //     int left = 0;                 // Pointer starting from the beginning of the array
-    //     int right = arr.size() - 1;  // Pointer starting from the end of the array
-        
-    //     // Continue swapping until the two pointers meet
-    //     while (left < right) {
-    //         // Swap the elements at left and right indices
-    //         swap(arr[left], arr[right]);
-    //         left++;   // Move the left pointer to the right
-    //         right--;  // Move the right pointer to the left
-    //     }
-    // }
-
 };
 
 //{ Driver Code Starts.
@@ -57,7 +35,7 @@ int main() {
         }
 
         Solution ob;
-        arr = ob.reverseArray(arr);
+        ob.reverseArray(arr);
         for (int i = 0; i < arr.size(); i++) {
             cout << arr[i] << " ";
         }
